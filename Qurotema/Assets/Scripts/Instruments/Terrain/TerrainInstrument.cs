8a627f2e276b6484a876bd537d3fb2e12a390685
vi <reducedprocess@gmail.com>
2026-04-09 02:16:57 -0400

    further improve terrain flash

--- Qurotema/Assets/Scripts/Instruments/Terrain/TerrainInstrument.cs
+++ Qurotema/Assets/Scripts/Instruments/Terrain/TerrainInstrument.cs
@@ -2,26 +2,26 @@ using UnityEngine;
 
 public class TerrainInstrument : MonoBehaviour {
 
-    [Header("References")]
-    public Material terrainMaterial;
+	[Header("References")]
+	public Material terrainMaterial;
 
-    [Header("Dynamics")]
-    public AnimationCurve flashCurve;
-    private float playHead = 0f;
-    private float speed = 5f;
+	[Header("Dynamics")]
+	public AnimationCurve flashCurve;
+	private float playHead = 0f;
+	private float speed = 5f;
 
-    private float intensityMultiplier = 20f;
-    private float value = 0f;
+	private float intensityMultiplier = 20f;
+	private float value = 0f;
 
-    void Update() {
-        playHead = playHead + speed * Time.deltaTime;
+	void Update() {
+		playHead = playHead + speed * Time.deltaTime;
 
-        terrainMaterial.SetFloat("_Play_Intensity", flashCurve.Evaluate(playHead) * intensityMultiplier);
-        terrainMaterial.SetFloat("_Play_Value", value);
-    }
+		terrainMaterial.SetFloat("_Play_Intensity", flashCurve.Evaluate(playHead) * intensityMultiplier);
+		terrainMaterial.SetFloat("_Play_Value", value);
+	}
 
-    public void play(float v) {
-        playHead = 0f;
-        value = v;
-    }
+	public void play(float v) {
+		playHead = 0f;
+		value = v;
+	}
 }
--- Qurotema/Assets/Scripts/Player/TerrainBehavior.cs
+++ Qurotema/Assets/Scripts/Player/TerrainBehavior.cs
@@ -3,28 +3,28 @@ using UnityEngine.InputSystem;
 
 public class TerrainBehavior : MonoBehaviour {
 
-    [Header("References")]
+	[Header("References")]
 	public Transform cursor;
 
-    [Header("Dynamics")]
+	[Header("Dynamics")]
 	public LayerMask mask;
 
-    //input
+	//input
 	private InputAction cursorAction;
 	private InputAction interactAction;
 
-    void Start() {
+	void Start() {
 		cursorAction = InputSystem.actions.FindAction("Cursor");
 		interactAction = InputSystem.actions.FindAction("Interact");
 	}
 
-    void Update() {
+	void Update() {
 		if (cursorAction.IsPressed() && interactAction.WasPressedThisFrame()) {
 			RaycastHit hit;
 			if (Physics.Raycast(transform.position, (cursor.position - transform.position).normalized, out hit, Mathf.Infinity, ~mask)) {
 				if (hit.collider.tag == "Terrain") {
 					float angle = Vector3.Dot(hit.normal, (transform.position - hit.point).normalized);
-                    hit.collider.gameObject.GetComponent<TerrainInstrument>().play(angle);
+					hit.collider.gameObject.GetComponent<TerrainInstrument>().play(angle);
 				}
 			}
 		}
