27951e1d7d89568361946c37f7c3c8f84f133fd2
vi <reducedprocess@gmail.com>
2026-08-01 17:33:47 -0400

    prevent metal from glowing and improve glow easing consistency on monoliths

--- Qurotema/Assets/Scripts/Monolith/MonolithInstrument.cs
+++ Qurotema/Assets/Scripts/Monolith/MonolithInstrument.cs
@@ -2,38 +2,47 @@ using UnityEngine;
 
 public class MonolithInstrument : MonoBehaviour {
 
-    private MeshRenderer ren;
-    private MaterialPropertyBlock mpb;
+	private MeshRenderer ren;
+	private MaterialPropertyBlock mpb;
 
-    private float intensity = 0f;
-    private float targetIntensity = 0f;
-    private float intensityFalloff = 100f;
-    private float intensityMax = 1000f;
+	private float intensity = 0f;
+	private float targetIntensity = 0f;
+	private float intensityFalloff = 150f;
+	private float intensityMax = 1000f;
 
-    private float value = 0f;
-    private float targetValue = 0f;
-    
-    private float ease = 0.5f;
+	private float value = 0f;
+	private float targetValue = 0f;
 
-    void Start() {
-        ren = GetComponent<MeshRenderer>();
-        mpb = new MaterialPropertyBlock();
-    }
+	private float epsilon = 0.001f;
 
-    void Update() {
-        targetIntensity = Mathf.Clamp(targetIntensity - intensityFalloff * Time.deltaTime, 0f, intensityMax);
+	private float ease = 1f;
 
-        intensity = Mathf.Lerp(intensity, targetIntensity, ease * Time.deltaTime);
-        value = Mathf.Lerp(value, targetValue, ease * Time.deltaTime);
+	void Start() {
+		ren = GetComponent<MeshRenderer>();
+		mpb = new MaterialPropertyBlock();
+	}
 
-        mpb.SetFloat("_Emission_Intensity", intensity);
-        mpb.SetFloat("_Emission_Value", value);
+	void Update() {
+		targetIntensity = Mathf.Clamp(targetIntensity - intensityFalloff * Time.deltaTime, 0f, intensityMax);
 
-        ren.SetPropertyBlock(mpb);
-    }
+		intensity = Mathf.Lerp(intensity, targetIntensity, ease * Time.deltaTime);
+		value = Mathf.Lerp(value, targetValue, ease * Time.deltaTime);
 
-    public void play(float v) {
-        targetIntensity = intensityMax;
-        targetValue = v;
-    }
+		mpb.SetFloat("_Emission_Intensity", intensity);
+		mpb.SetFloat("_Emission_Value", value);
+
+		ren.SetPropertyBlock(mpb);
+	}
+
+	public void play(float v) {
+		targetIntensity = intensityMax;
+		targetValue = logTransform(v); //transform to log space for more natural perceptive brightness
+	}
+
+	private float logTransform(float v) {
+		float minLog = Mathf.Log(epsilon);
+		float vLog = Mathf.Log(Mathf.Max(v, epsilon));
+		//map logFloor..1 to 0..1
+		return (vLog - minLog) / (0f - minLog);
+	}
 }
